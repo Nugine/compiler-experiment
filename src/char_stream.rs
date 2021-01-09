c3a12458c749a9c49c7ff8727b79c56afd4d6728
Nugine <nugine@foxmail.com>
2021-01-09 23:02:39 +0800

    add common types

---
+++ src/char_stream.rs
@@ -0,0 +1,156 @@
+use super::source_file::SourceFile;
+use super::span::Pos;
+
+use std::rc::Rc;
+
+pub struct CharStream {
+    content: Rc<[char]>,
+    file_path: Option<Rc<str>>,
+
+    idx: usize,
+    byte_pos: usize,
+    lineno: usize,
+    column: usize,
+}
+
+impl CharStream {
+    pub fn new(src: SourceFile) -> Self {
+        Self {
+            content: src.content,
+            file_path: src.file_path,
+
+            idx: 0,
+            byte_pos: 0,
+            lineno: 1,
+            column: 0,
+        }
+    }
+
+    pub fn cur(&self) -> Option<char> {
+        if self.idx == 0 {
+            return None;
+        }
+        Some(self.content[self.idx - 1])
+    }
+
+    pub fn peek(&self) -> Option<char> {
+        self.content.get(self.idx).copied()
+    }
+
+    pub fn peek2(&self) -> Option<char> {
+        self.content.get(self.idx + 1).copied()
+    }
+
+    pub fn next_char(&mut self) -> Option<char> {
+        if self.idx > 0 {
+            let cur = self.content.get(self.idx - 1).copied()?;
+            let char_len = cur.len_utf8();
+            self.byte_pos += char_len;
+            if cur == '\n' {
+                self.lineno += 1;
+                self.column = 0;
+            }
+        }
+        self.column += 1;
+        let ch = self.content.get(self.idx).copied()?;
+        self.idx += 1;
+        Some(ch)
+    }
+
+    pub fn consume1(&mut self) -> char {
+        self.next().unwrap()
+    }
+
+    pub fn pos(&self) -> Pos {
+        Pos {
+            byte_pos: self.byte_pos,
+            lineno: self.lineno,
+            column: self.column,
+        }
+    }
+
+    pub fn file_path(&self) -> Option<&Rc<str>> {
+        self.file_path.as_ref()
+    }
+}
+
+impl Iterator for CharStream {
+    type Item = char;
+    fn next(&mut self) -> Option<char> {
+        self.next_char()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn dummy_char_stream(content: &str) -> CharStream {
+        CharStream::new(SourceFile::in_memory(content))
+    }
+
+    #[test]
+    fn char_stream() {
+        {
+            let mut chars = dummy_char_stream("");
+            assert_eq!(chars.cur(), None);
+            assert_eq!(chars.next(), None);
+        }
+        {
+            let mut chars = dummy_char_stream("12");
+            assert_eq!(chars.cur(), None);
+
+            assert_eq!(chars.peek(), Some('1'));
+            assert_eq!(chars.next(), Some('1'));
+            assert_eq!(chars.cur(), Some('1'));
+            assert_eq!(chars.pos(), Pos::new(0, 1, 1));
+
+            assert_eq!(chars.peek(), Some('2'));
+            assert_eq!(chars.next(), Some('2'));
+            assert_eq!(chars.cur(), Some('2'));
+            assert_eq!(chars.pos(), Pos::new(1, 1, 2));
+
+            assert_eq!(chars.next(), None);
+            assert_eq!(chars.pos(), Pos::new(2, 1, 3));
+        }
+        {
+            let mut chars = dummy_char_stream("1\n2");
+            assert_eq!(chars.cur(), None);
+
+            assert_eq!(chars.peek(), Some('1'));
+            assert_eq!(chars.next(), Some('1'));
+            assert_eq!(chars.cur(), Some('1'));
+            assert_eq!(chars.pos(), Pos::new(0, 1, 1));
+
+            assert_eq!(chars.peek(), Some('\n'));
+            assert_eq!(chars.next(), Some('\n'));
+            assert_eq!(chars.cur(), Some('\n'));
+            assert_eq!(chars.pos(), Pos::new(1, 1, 2));
+
+            assert_eq!(chars.peek(), Some('2'));
+            assert_eq!(chars.next(), Some('2'));
+            assert_eq!(chars.cur(), Some('2'));
+            assert_eq!(chars.pos(), Pos::new(2, 2, 1));
+
+            assert_eq!(chars.next(), None);
+            assert_eq!(chars.pos(), Pos::new(3, 2, 2));
+        }
+        {
+            let mut chars = dummy_char_stream("好，很有精神");
+            assert_eq!(chars.cur(), None);
+
+            assert_eq!(chars.peek(), Some('好'));
+            assert_eq!(chars.next(), Some('好'));
+            assert_eq!(chars.cur(), Some('好'));
+            assert_eq!(chars.pos(), Pos::new(0, 1, 1));
+
+            assert_eq!(chars.peek(), Some('，'));
+            assert_eq!(chars.next(), Some('，'));
+            assert_eq!(chars.cur(), Some('，'));
+            assert_eq!(chars.pos(), Pos::new(3, 1, 2));
+
+            assert_eq!(chars.next(), Some('很'));
+            assert_eq!(chars.pos(), Pos::new(6, 1, 3));
+        }
+    }
+}
---
+++ src/span.rs
@@ -0,0 +1,40 @@
+use std::ops::Range;
+use std::rc::Rc;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct LineColumn {
+    pub line: usize,
+    pub column: usize,
+}
+
+#[derive(Debug, Clone)]
+pub struct Span {
+    pub byte_range: Range<usize>,
+    pub lc_range: Range<LineColumn>,
+    pub file_path: Option<Rc<str>>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Pos {
+    pub byte_pos: usize,
+    pub lineno: usize,
+    pub column: usize,
+}
+
+impl Pos {
+    pub fn new(byte_pos: usize, lineno: usize, column: usize) -> Self {
+        Pos {
+            byte_pos,
+            lineno,
+            column,
+        }
+    }
+
+    pub fn add1(self) -> Self {
+        Self {
+            byte_pos: self.byte_pos + 1,
+            lineno: self.lineno,
+            column: self.column + 1,
+        }
+    }
+}
